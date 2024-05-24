1f4e18929773da0286a49482f73c2f4a7b8701e8
MidhunMtech <midhun.m@techversantinfotech.com>
2024-05-24 18:04:55 +0530

    DatePicker using jQuery

---
+++ DatePicker jQ/js/jquery-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#datepicker").datepicker({        
+		dateFormat: 'dd-mm-yy',
+        	defaultDate: '01-01-2024'
+	});
+	$("#datepicker").datepicker("show");
+})
